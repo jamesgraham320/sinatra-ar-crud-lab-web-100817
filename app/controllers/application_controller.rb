fd9cfa7edba529f98a05d2cc92ec2cd9ad91092c
github username <github email address>
2017-11-01 15:38:48 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
+  set :method_override, 'true'
+
 
   configure do
     set :public_folder, 'public'
@@ -8,6 +10,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do 
-    
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    erb :posts
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+  
+  get "/posts/:id" do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get "/posts/:id/edit" do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+  
+  post '/posts' do
+    @post = Post.create(params)
+    #@post = Post.create(name: params[:name], content: params[:content])
+    erb :posts
+  end
+  
+  get '/index' do 
+    erb :index
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(
+      name: params[:name],
+      content: params[:content])
+    redirect "/posts/#{@post.id}"
+  end
+
+  delete "/posts/:id/delete" do
+    post = Post.find(params[:id])
+    @post_name = post.name
+    post.destroy
+    erb :delete
   end
 end
